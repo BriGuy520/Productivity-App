5548721e14b0b3b60a1003e36ffdf244b7e8df22
Brian Falasz <bfalasz@yahoo.com>
2021-10-30 22:06:09 -0500

    was able to generate text on the screen in templage

--- to_do_list/list/models.py
+++ to_do_list/list/models.py
@@ -8,6 +8,9 @@ class User(models.Model):
 
   first_name = models.CharField(max_length=20)
   last_name = models.CharField(max_length=20)
+
+  def __str__(self):
+    return f'{self.first_name} {self.last_name}'
   
 
 
@@ -15,4 +18,7 @@ class Task(models.Model):
   
   task_title = models.CharField(max_length=100)
   task_summary = models.TextField(max_length=1000)
+  task_status = models.CharField(max_length=3)
+
+
 
