8b33f8852139eea7101d9e4079bb3ad82220e161
Brian Falasz <brianfalasz@gmail.com>
2021-11-03 22:42:44 -0500

    finished the login feature

--- to_do_list/to_do_list/urls.py
+++ to_do_list/to_do_list/urls.py
@@ -19,7 +19,7 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path('', include('list.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
+    path('', include('list.urls')),
 ]
