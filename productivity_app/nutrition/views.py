d601dc40aa58cfe6bc0caa780b1c358b4e164e0d
Brian Falasz <brianfalasz@gmail.com>
2021-11-16 20:51:54 -0600

    was able to set up my template to show a decent amount of nutritional data

--- productivity_app/nutrition/views.py
+++ productivity_app/nutrition/views.py
@@ -9,9 +9,9 @@ import requests
 # Create your views here.
 def search_food(request):
 
-    search_term = 'cheddar cheese'  
+    search_term = 'milk'  
 
-    r = requests.get(f'https://api.edamam.com/api/food-database/v2/parser?app_id={settings.EDAMAM_APP_ID}&app_key={settings.EDAMAM_API_KEY}&ingr=milk&category=generic-foods')
+    r = requests.get(f'https://api.edamam.com/api/food-database/v2/parser?app_id={settings.EDAMAM_APP_ID}&app_key={settings.EDAMAM_API_KEY}&ingr={search_term}&nutrition-type=logging&calories=0-1000&category=generic-foods')
     foods = r.json()
     print('Status: {}'.format(r.status_code))
 
